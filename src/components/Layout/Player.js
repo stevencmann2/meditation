984895aaf58dc6b9393a4fd5cfc76d6aaa3ec907
Steven Mann <stevencmann2@gmail.com>
2020-04-28 16:00:53 -0500

    need to develop system for measuring svg path, may need to make seperate component

--- src/components/Layout/Player.js
+++ src/components/Layout/Player.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Audio from '../Design/Audio'
 import '../../assets/css/style.css';
 
 
 const Player = () =>{
 
+    const outlineRef = useRef(null);
+   
+    // const outlineLength = outline.getTotalLength()
+
+    console.log(outlineRef)
     return(
         <div className="player-container">
             <Audio 
@@ -13,36 +18,40 @@ const Player = () =>{
             <img src={require("../../assets/images/svg/play.svg")} alt="play" className="play" />
             <svg 
                 className="track-outline"
-                width="453" 
-                height="453" 
+                width={453}
+                height={453} 
                 viewBox="0 0 453 453" 
                 fill="none" 
                 xmlns="http://www.w3.org/2000/svg"
             >
             
                 <circle 
-                    cx="226.5" 
-                    cy="226.5" 
-                    r="216.5" 
+                    cx={226.5} 
+                    cy={226.5} 
+                    r={216.5}
                     stroke="white" 
-                    strokeWidth="20"
+                    strokeWidth={20}
                 />
             </svg>
             <svg 
                 className="moving-outline"
-                width="453" 
-                height="453" 
+                width={453} 
+                height={453}
                 viewBox="0 0 453 453" 
                 fill="none" 
                 xmlns="http://www.w3.org/2000/svg"
+                
             >
                 <circle 
-                    cx="226.5" 
-                    cy="226.5" 
-                    r="216.5" 
+                    ref={outlineRef}
+                    cx={226.5} 
+                    cy={226.5} 
+                    r={216.5}
                     stroke="#018EBA" 
-                    strokeWidth="20"
+                    strokeWidth={20}
+                    
                 />
+               
             </svg>
             <h3 className="time-display">0:00</h3>
 
